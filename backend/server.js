23b1308571f2133e9b982b5868ba5c9903d66aff
Corina717 <corina.simionescu12@gmail.com>
2024-10-21 21:55:59 +0300

    4

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
       ? // ? "https://food-tracker-frontend.onrender.com"
-        "/*"
+        "*"
       : "http://localhost:5173",
 };
 
